90012e339a5d4703b0ab79fa4b6acdd511d4bfcf
syedsaadahmed1 <ahmedfn@amazon.com>
2022-03-23 23:47:42 -0400

    My first push-to-deploy with CodePipeline

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ import Link from "next/link";
 import Logo from "./logo";
 
 export const siteTitle = "Hello App Runner and Copilot!";
-export const topPageMessage = "Let's Get Started!";
+export const topPageMessage = "Updated Welcome Message!";
 
 export default function Layout({ children, home }) {
   return (
